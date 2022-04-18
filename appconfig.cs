56cea622810ab230e9ce234e670cc5158363fa1f
Mark Harrison <mark@harrison.ws>
2022-04-18 22:32:08 +0100

    Updates to .NET 6.0

--- appconfig.cs
+++ appconfig.cs
@@ -8,33 +8,12 @@ namespace ColoursWeb
 {
     public class AppConfig
     {
-        //private string _ThingsAPIUrl;
         private string _AdminPWVal;
         public AppConfig(IConfiguration _config)
         {
-            //_ThingsAPIUrl = _config.GetValue<string>("ThingsAPIUrl");
-            //if (!_ThingsAPIUrl.EndsWith("/"))
-            //{
-            //    _ThingsAPIUrl += "/";
-            //}
-
             _AdminPWVal = _config.GetValue<string>("AdminPW");
         }
-
-        //public string ThingsAPIUrl
-        //{
-        //    get => this._ThingsAPIUrl;
-        //    set
-        //    {
-        //        this._ThingsAPIUrl = value;
-        //        if (!_ThingsAPIUrl.EndsWith("/"))
-        //        {
-        //            _ThingsAPIUrl += "/";
-        //        }
-        //    }
-        //}
-
-    
+ 
         public string AdminPW
         {
             get => this._AdminPWVal;
